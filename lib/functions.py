3b960925438d0e7c7cfefb54e165623b0ee6146f
Shaib Echesa Makokha <echesaswahib@gmail.com>
2024-08-27 00:38:36 +0300

    functions in python

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,29 @@
 
 def greet_programmer():
     pass
+def greet_programmer():
+    print("Hello, programmer!")
+
 
 def greet(name):
     pass
+def greet(name):
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
     pass
+def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
     pass
+def add(a, b):
+    return a + b
+
 
 def halve(number):
     pass
+def halve(number):
+    return number / 2
